bbc3df80a36815632f706d55bcbf2ed04395be5b
Donald Ward <4127162+dwward@users.noreply.github.com>
2021-07-27 22:08:34 -0400

    Added filtering by belt color

--- src/App.js
+++ src/App.js
@@ -5,20 +5,33 @@ import TechniqueList from "./TechniqueList";
 import {Typography} from "@material-ui/core";
 import Data from './data/syllabus.json';
 import React, {useState} from "react";
+import CategoryFilter from "./CategoryFilter";
 
 
 function App() {
 
     const originalSyllabusData = Data;
     const [syllabus, setSyllabus] = useState(Data);
-    const [selectedBelt, setSelectedBelt] = useState(null);
+    const [selectedBelt, setSelectedBelt] = useState('');
+    const [selectedCategory, setSelectedCategory] = useState('');
+
     const filterBelt = (color) => {
+        if (color == '') {
+            setSelectedBelt('');
+            setSyllabus(originalSyllabusData);
+            return;
+        }
         setSelectedBelt(color);
         let tmpSyllabus = Object.assign({}, originalSyllabusData);
-        let filterTechs = tmpSyllabus.techniques.filter(t => t.belt.toUpperCase() == color.toUpperCase());
+        let filterTechs = tmpSyllabus.techniques.filter(t => t.belt.toUpperCase() === color.toUpperCase());
         tmpSyllabus.techniques = filterTechs;
         setSyllabus(tmpSyllabus);
     }
+
+    const filterCategory = (cat) => {
+
+    }
+
     const filterTechniques = () => {
         // call setstate of child should cause re-render
         // techniquelist.setstate(...)
@@ -31,8 +44,8 @@ function App() {
                 <TechniqueFilter />
                 {/*<img src={'kblogo-lg.jpg'} class="kblogo" />*/}
             </header>
-            Selected Belt: {selectedBelt}
-            <BeltFilter setSelectedBelt={filterBelt} />
+            {/*<CategoryFilter category={selectedCategory} filterCategory={filterCategory} />*/}
+            <BeltFilter beltColor={selectedBelt} filterBelt={filterBelt} />
             <TechniqueList syllabus={syllabus} />
         </div>
 );
--- src/BeltFilter.js
+++ src/BeltFilter.js
@@ -1,22 +1,43 @@
 import React from "react";
 import Data from './data/syllabus.json';
-import {Button} from "@material-ui/core";
+import {FormControl, InputLabel, MenuItem, Select} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
 
 export default function BeltFilter(props) {
 
+    const beltTestColors = Data.belts.slice(1, -1);
+    const handleChange = (event) => {
+        let tmpBelt = event.target.value;
+        props.filterBelt(tmpBelt);
+    };
+
+    const useStyles = makeStyles((theme) => ({
+        formControl: {
+            margin: theme.spacing(1),
+            minWidth: 120,
+        },
+        selectEmpty: {
+            marginTop: theme.spacing(2),
+        },
+    }));
+    const classes = useStyles();
+
     return (
-        <ul>
-            {
-                Data.belts.map(b => {
-                    return (
-                        <Button variant="outlined"
-                                key={b}
-                                onClick={() => props.setSelectedBelt(b)}
-                        >{b}</Button>
-                    )
-                })
-            }
-        </ul>
+        <FormControl variant="filled" className={classes.formControl}>
+            <InputLabel htmlFor="filled-age-native-simple">Belt Test</InputLabel>
+            <Select
+                    value={props.beltColor}
+                    onChange={handleChange}
+            >
+                <MenuItem value=''>Show All</MenuItem>
+                {
+                    beltTestColors.map(curr => {
+                        return <MenuItem value={curr}>{curr}</MenuItem>
+                    })
+                }
+            </Select>
+        </FormControl>
+
     )
 }
 
